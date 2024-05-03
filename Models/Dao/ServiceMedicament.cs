462b2e24b1840c951f9203451b6a14572e16bb30
marjaneAbk <marjane.aberkan@lmdsio.fr>
2024-05-03 16:02:53 +0200

    GetTousLesMedicaments

--- Models/Dao/ServiceMedicament.cs
+++ Models/Dao/ServiceMedicament.cs
@@ -13,8 +13,8 @@ namespace WebGsbMedicament.Models.Dao
 			Serreurs er = new Serreurs("Erreur sur la lecture des medicaments", "Medicament.getMedicament()");
 			try
 			{
-				string mysql = "Select id_medicament, medicament.id_famille, depot_legal, nom_commercial, effets, contre_indication, prix_echantillon "
-					+ " from medicament join famille on medicament.id_famille = famille.id_famille";
+				string mysql = "Select id_medicament, medicament.id_famille, nom_commercial, effets, contre_indication, prix_echantillon, qte_dosage, unite_dosage, posologie, lib_type_individu "
+					+ " from medicament join famille on medicament.id_famille = famille.id_famille join prescrire on prescrire.id_medicament = famille.id_famille join dosage on prescrire.id_dosage = dosage.id_dosage join type_individu on prescrire.id_type_individu=type.individu.id_type_individu  ";
 
 				mesMedicaments = DBInterface.Lecture(mysql, er);
 
