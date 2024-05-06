82ecb2eea7ebdbf1c1b69124e71e02021b9af448
marjaneAbk <marjane.aberkan@lmdsio.fr>
2024-05-06 10:47:51 +0200

    lister

---
+++ Controllers/MedicamentController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebGsbMedicament.Models.Dao;
+using WebGsbMedicament.Models.MesExceptions;
+
+namespace WebGsbMedicament.Controllers
+{
+	public class MedicamentController : Controller
+	{
+		public IActionResult Index()
+		{
+			System.Data.DataTable mesMedicaments = null;
+
+			try
+			{
+				mesMedicaments = ServiceMedicament.GetTousLesMedicaments();
+			}
+			catch (MonException e)
+			{
+				ModelState.AddModelError("Erreur", "Erreur lors de la recuperation des medicaments : " + e.Message);
+			}
+			return View(mesMedicaments);
+		}
+	}
+}
--- Models/Dao/ServiceMedicament.cs
+++ Models/Dao/ServiceMedicament.cs
@@ -13,17 +13,19 @@ namespace WebGsbMedicament.Models.Dao
 			Serreurs er = new Serreurs("Erreur sur la lecture des medicaments", "Medicament.getMedicament()");
 			try
 			{
-				string mysql = "Select id_medicament, medicament.id_famille, nom_commercial, effets, contre_indication, prix_echantillon, qte_dosage, unite_dosage, posologie, lib_type_individu "
-					+ " from medicament join famille on medicament.id_famille = famille.id_famille join prescrire on prescrire.id_medicament = famille.id_famille join dosage on prescrire.id_dosage = dosage.id_dosage join type_individu on prescrire.id_type_individu=type.individu.id_type_individu  ";
+				string mysql = "Select medicament.id_medicament, medicament.id_famille, medicament.nom_commercial, medicament.effets, medicament.contre_indication, medicament.prix_echantillon, dosage.qte_dosage, dosage.unite_dosage, prescrire.posologie, type_individu.lib_type_individu "
+					+ " from medicament join famille on medicament.id_famille = famille.id_famille join prescrire on prescrire.id_medicament = famille.id_famille "
+					+ " join dosage on prescrire.id_dosage = dosage.id_dosage join type_individu on prescrire.id_type_individu=type_individu.id_type_individu  ";
 
 				mesMedicaments = DBInterface.Lecture(mysql, er);
 
 			}
 			catch (MonException e)
 			{
-				throw new MonException(er.MessageUtilisateur(), er.MessageApplication());
-			}
+				throw new MonException(er.MessageUtilisateur(), er.MessageApplication(), e.Message);
+			} 
 		}
+
 
 	}
 }
